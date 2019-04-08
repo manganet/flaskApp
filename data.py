967cf9d70cd4d543c950a38deb21ea08309f0ee7
manganet <manganet55@gmail.com>
2019-04-08 12:52:09 +0400

    flaskApp

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae numquam rerum corrupti voluptate nobis aut? Non, consequatur dicta facilis quibusdam distinctio, nulla error dolorem sit molestias laudantium architecto, veniam illo.',
+            'author': 'Moi meme',
+            'create_date': '08-04-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae numquam rerum corrupti voluptate nobis aut? Non, consequatur dicta facilis quibusdam distinctio, nulla error dolorem sit molestias laudantium architecto, veniam illo.',
+            'author': 'Toi Aussi',
+            'create_date': '09-04-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae numquam rerum corrupti voluptate nobis aut? Non, consequatur dicta facilis quibusdam distinctio, nulla error dolorem sit molestias laudantium architecto, veniam illo.',
+            'author': 'Lui Egalement',
+            'create_date': '10-04-2019'
+        }
+    ]
+    return articles
